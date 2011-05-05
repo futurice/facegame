bbae5a2aeabe4b098a85e6607cec005a7d662daf
ojar@futurice.com <ojar@futurice.com>
2011-05-05 12:17:20 +0300

    Removed deprecated code.

--- media/fgscript.js
+++ media/fgscript.js
@@ -1,8 +1,3 @@
-/*var correctAnswers = 0;
-var wrongAnswers = 0;
-var currentStreak = 0;
-var highestStreak = 0;
-var skips = 0;*/
 var skippable = true;
 var mute = true;
 
@@ -45,10 +40,7 @@ $(document).ready(function()
 	$("#soundHandle1").attr('preload', 'auto');
 	$("#soundHandle2").attr('preload', 'auto');
 	soundHandle1.src = '/facegame/static/sounds/correct.ogg';
-	/*$(soundHandle1).bind("ended", function()
-	{
-		$('#nameform').submit();
-	});*/
+
 	soundHandle2.src = '/facegame/static/sounds/wrong.ogg';
 
 	$('.muteimg').bind("click", function(event)
@@ -63,19 +55,6 @@ $(document).ready(function()
 			$('.muteimg').attr('src', '/facegame/static/images/muteon.png');
 		}
 	});
-
-	/*$('.helpimg').bind("click", function(event)
-	{
-		toggleTooltips();
-	});*/
-
-	/*$('.correctimg').tipsy({trigger: 'manual', html: true, gravity: 'se'});
-	$('.wrongimg').tipsy({trigger: 'manual', html: true, gravity: 'sw'});
-	$('.rowimg').tipsy({trigger: 'manual', html: true, gravity: 'ne'});
-	$('.skipimg').tipsy({trigger: 'manual', html: true, gravity: 'nw'});
-	$('.muteimg').tipsy({trigger: 'manual', html: true, gravity: 's'});
-	$('.helpimg').tipsy({trigger: 'manual', html: true, gravity: 'w'});
-	$('.logoimg').tipsy({trigger: 'manual', html: true, gravity: 'e'});*/
 
 	$('.correctimg').tipsy();
 	$('.wrongimg').tipsy();
@@ -98,33 +77,6 @@ function response(responseText)
 		initialize();
 	});
 }
-
-/*function toggleTooltips()
-{
-	if (tooltips == false)
-	{
-		$('.correctimg').tipsy("show");
-		$('.wrongimg').tipsy("show");
-		$('.rowimg').tipsy("show");
-		$('.skipimg').tipsy("show");
-		$('.muteimg').tipsy("show");
-		$('.helpimg').tipsy("show");
-		$('.logoimg').tipsy("show");
-		tooltips = true;
-		return false;
-	} else
-	{
-		$('.correctimg').tipsy("hide");
-		$('.wrongimg').tipsy("hide");
-		$('.rowimg').tipsy("hide");
-		$('.skipimg').tipsy("hide");
-		$('.muteimg').tipsy("hide");
-		$('.helpimg').tipsy("hide");
-		$('.logoimg').tipsy("hide");
-		tooltips = false;
-		return false;
-	}
-}*/
 
 function deteleconfirm()
 {
@@ -232,10 +184,6 @@ function initialize()
 				{
 					$(".correctimg").animate({"width": "-=6px", "height": "-=6px"}, 350);					
 				});
-				/*if (mute == true)
-				{
-					$('#nameform').submit();
-				}*/
 				$.get('/facegame/jsonform/?ajax=true&random='+Math.random(), function(form)
 				{
 					$('#face').fadeOut(400);
@@ -267,72 +215,6 @@ function initialize()
 		});
 		return false;
 		
-
-		/*if ($(this).find("input[type=radio]").val() == rnCorrect)
-		{
-			if (mute == false)
-			{
-				soundHandle1.load();
-				soundHandle1.play();
-			}
-			$("li").unbind('click');
-			$(".skipimg").fadeTo(500, 0.3).unbind('click');
-			correctAnswers += 1;
-			currentStreak += 1;
-			if (currentStreak > highestStreak)
-			{
-				highestStreak = currentStreak;
-			}
-			if (skippable == true)
-			{
-				skippable = false;
-			}
-			$('#correctnum').html(correctAnswers);
-			$('#rownum').html(currentStreak + ", " + highestStreak);
-			$(this).find("input[type=radio]").attr("checked", "checked");
-			$('#nameform').fadeOut(600);
-			$('#face').fadeOut(600, function()
-			{
-				$(this).attr('src', '/facegame/static/images/loader.gif');
-				$(this).fadeIn(400);
-			});
-			$(".correctimg").animate({"width": "+=6px", "height": "+=6px"}, 300, function()
-			{
-				$(".correctimg").animate({"width": "-=6px", "height": "-=6px"}, 350);					
-			});
-			if (mute == true)
-			{
-				$('#nameform').submit();
-			}
-			return false;
-		}
-		else
-		{
-			$.post('/update/?ajax=true&random='+Math.random(), {'test': 'test'}, function(data){
-				alert(data.valid);
-			});
-			if (mute == false)
-			{
-				soundHandle2.load();
-				soundHandle2.play();
-			}
-			$(this).unbind('click');
-			$(".skipimg").fadeTo(500, 0.3).unbind('click');
-			wrongAnswers += 1;
-			if (skippable == true)
-			{
-				skippable = false;
-			}
-			$('#wrongnum').html(wrongAnswers);
-			currentStreak = 0;
-			$('#rownum').html(currentStreak + ", " + highestStreak);
-			$(this).fadeTo(700, 0.35);
-			$(".wrongimg").animate({"width": "+=5px", "height": "+=5px"}, 300, function()
-			{
-				$(".wrongimg").animate({"width": "-=5px", "height": "-=5px"}, 350);
-			});
-			return false;
-		}*/
 	});
 
 	/*var options = {target: '#output', success: response};
