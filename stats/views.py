c9c9676da4aa27c930d4f2fa6c13ffcd89503926
ojar@futurice.com <ojar@futurice.com>
2011-05-09 10:20:08 +0300

    Hall of Fame view

---
+++ stats/views.py
@@ -0,0 +1,30 @@
+from django.core.cache import cache
+from django.core.context_processors import csrf
+from django.core.paginator import Paginator
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect, HttpResponse
+from django.template import RequestContext
+from django.template.loader import render_to_string
+from django.conf import settings
+from fumapi import read, read_list
+from django import forms
+from faceguessing.models import Player, UserStats
+from faceguessing.views import __read_fum_user
+import random
+import json
+import hashlib
+import os
+
+
+def hall_of_fame(request):
+    try:
+        player = UserStats.objects.get(username=request.user.username)
+    except:
+        player = None
+    hall_of_fame_list = UserStats.objects.order_by('-success')
+    hall_of_fame = []
+    for item in hall_of_fame_list:
+         user = __read_fum_user(item.username)
+         hall_of_fame.append({'stats': item, 'user': user})
+    return render_to_response("hall_of_fame.html", {"player": player, 'hall_of_fame': hall_of_fame }, context_instance=RequestContext(request))
+
--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 from faceguessing.views import index, updatestats, jsonform, get_user_image
+from stats.views import hall_of_fame
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -12,16 +13,13 @@ urlpatterns = patterns('',
     url(r'^updatestats/', updatestats),
     url(r'^jsonform/', jsonform),
     url(r'^image/current/', get_user_image),
+    url(r'^hall_of_fame$', hall_of_fame),
+
 	
-	(r'^facegame/static/(?P<path>.*)$', 'django.views.static.serve',
-	{'document_root': settings.STATIC_ROOT}),
-	#(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-	#	{'document_root': settings.STATIC_ROOT}),
+    (r'^facegame/static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT}),
+    #(r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+    # {'document_root': settings.STATIC_ROOT}),
 
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
