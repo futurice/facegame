4a0df939826dd7fe68db9588af1129df90d0ad8f
Jussi Vaihia <jussi.vaihia@futurice.com>
2014-04-10 14:44:22 +0300

    prod settings

--- facegame/settings/prod.py
+++ facegame/settings/prod.py
@@ -6,4 +6,11 @@ ADMINS = (
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtpgw.futurice.com"
+EMAIL_PORT = 25
+
+STATIC_URL = '/facegame-static/'
+MEDIA_URL = '/facegame-static/'
+
 URLS_BASE = '/facegame/'
