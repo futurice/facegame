f86f837e749aee7bd102d8027dd2e8fd0d528a0b
Jussi Vaihia <jussi.vaihia@futurice.com>
2014-04-11 12:08:21 +0300

    prod settings

--- facegame/settings/prod.py
+++ facegame/settings/prod.py
@@ -14,6 +14,11 @@ STATIC_URL = '/facegame-static/'
 MEDIA_URL = '/facegame-media/'
 
 # PROJECT_ROOT -> DEPLOY_ROOT
+# ROOT/releases/release/settings.prod
+PACKAGE_ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', '..', '..', '..'))
+PROJECT_ROOT = os.path.normpath(os.path.join(PACKAGE_ROOT, 'www'))
+DEPLOY_ROOT = PACKAGE_ROOT
+
 MEDIA_ROOT = os.path.join(DEPLOY_ROOT, 'media') + os.sep
 STATIC_ROOT = os.path.join(DEPLOY_ROOT, 'static') + os.sep
 DATABASES['default']['NAME'] = os.path.abspath(os.path.join(DEPLOY_ROOT, 'sqlite.db'))
